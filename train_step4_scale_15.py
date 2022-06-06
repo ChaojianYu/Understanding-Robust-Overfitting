3d1723bb3f362001188b06344b8a7b67c9b5940b
ChaojianYu <849976164@qq.com>
2022-06-06 17:00:08 +0800

    Add files via upload

---
+++ train_step4_scale_15.py
@@ -0,0 +1,370 @@
+import argparse
+import logging
+import sys
+import time
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import os
+
+from wideresnet import WideResNet
+from preactresnet import PreActResNet18
+from utils import *
+
+mu = torch.tensor(cifar10_mean).view(3, 1, 1).cuda()
+std = torch.tensor(cifar10_std).view(3, 1, 1).cuda()
+
+
+def normalize(X):
+    return (X - mu)/std
+
+
+upper_limit, lower_limit = 1,0
+
+
+def clamp(X, lower_limit, upper_limit):
+    return torch.max(torch.min(X, upper_limit), lower_limit)
+
+
+class Batches():
+    def __init__(self, dataset, batch_size, shuffle, set_random_choices=False, num_workers=0, drop_last=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.set_random_choices = set_random_choices
+        self.dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=True, shuffle=shuffle, drop_last=drop_last
+        )
+
+    def __iter__(self):
+        if self.set_random_choices:
+            self.dataset.set_random_choices()
+        return ({'input': x.to(device).float(), 'target': y.to(device).long()} for (x,y) in self.dataloader)
+
+    def __len__(self):
+        return len(self.dataloader)
+
+
+def attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts,
+               norm, early_stop=False):
+    max_loss = torch.zeros(y.shape[0]).cuda()
+    max_delta = torch.zeros_like(X).cuda()
+    for _ in range(restarts):
+        delta = torch.zeros_like(X).cuda()
+        if norm == "l_inf":
+            delta.uniform_(-epsilon, epsilon)
+        elif norm == "l_2":
+            delta.normal_()
+            d_flat = delta.view(delta.size(0),-1)
+            n = d_flat.norm(p=2,dim=1).view(delta.size(0),1,1,1)
+            r = torch.zeros_like(n).uniform_(0, 1)
+            delta *= r/n*epsilon
+        else:
+            raise ValueError
+        delta = clamp(delta, lower_limit-X, upper_limit-X)
+        delta.requires_grad = True
+        for _ in range(attack_iters):
+            output = model(normalize(X + delta))
+            if early_stop:
+                index = torch.where(output.max(1)[1] == y)[0]
+            else:
+                index = slice(None,None,None)
+            if not isinstance(index, slice) and len(index) == 0:
+                break
+            loss = F.cross_entropy(output, y)
+            loss.backward()
+            grad = delta.grad.detach()
+            d = delta[index, :, :, :]
+            g = grad[index, :, :, :]
+            x = X[index, :, :, :]
+            if norm == "l_inf":
+                d = torch.clamp(d + alpha * torch.sign(g), min=-epsilon, max=epsilon)
+            elif norm == "l_2":
+                g_norm = torch.norm(g.view(g.shape[0],-1),dim=1).view(-1,1,1,1)
+                scaled_g = g/(g_norm + 1e-10)
+                d = (d + scaled_g*alpha).view(d.size(0),-1).renorm(p=2,dim=0,maxnorm=epsilon).view_as(d)
+            d = clamp(d, lower_limit - x, upper_limit - x)
+            delta.data[index, :, :, :] = d
+            delta.grad.zero_()
+        all_loss = F.cross_entropy(model(normalize(X+delta)), y, reduction='none')
+        max_delta[all_loss >= max_loss] = delta.detach()[all_loss >= max_loss]
+        max_loss = torch.max(max_loss, all_loss)
+    return max_delta
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='PreActResNet18')
+    parser.add_argument('--batch-size', default=128, type=int)
+    parser.add_argument('--batch-size-test', default=128, type=int)
+    parser.add_argument('--data-dir', default='../cifar-data', type=str)
+    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--lr-schedule', default='piecewise')
+    parser.add_argument('--lr-max', default=0.1, type=float)
+    parser.add_argument('--attack', default='pgd', type=str, choices=['pgd', 'none'])
+    parser.add_argument('--epsilon', default=8, type=int)
+    parser.add_argument('--attack-iters', default=10, type=int)
+    parser.add_argument('--attack-iters-test', default=20, type=int)
+    parser.add_argument('--restarts', default=1, type=int)
+    parser.add_argument('--pgd-alpha', default=2, type=float)
+    parser.add_argument('--norm', default='l_inf', type=str, choices=['l_inf', 'l_2'])
+    parser.add_argument('--fname', default='cifar_model_step4_scale_15', type=str)
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--width-factor', default=10, type=int)
+    parser.add_argument('--resume', default=0, type=int)
+    parser.add_argument('--eval', action='store_true')
+    parser.add_argument('--chkpt-iters', default=1000, type=int)
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    if not os.path.exists(args.fname):
+        os.makedirs(args.fname)
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format='[%(asctime)s] - %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S',
+        level=logging.DEBUG,
+        handlers=[
+            logging.FileHandler(os.path.join(args.fname, 'eval.log' if args.eval else 'output.log')),
+            logging.StreamHandler()
+        ])
+
+    logger.info(args)
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+    transforms = [Crop(32, 32), FlipLR()]
+    
+    dataset = cifar10(args.data_dir)
+    train_set = list(zip(transpose(pad(dataset['train']['data'], 4)/255.),
+        dataset['train']['labels']))
+    train_set_x = Transform(train_set, transforms)
+    train_batches = Batches(train_set_x, args.batch_size, shuffle=True, set_random_choices=True, num_workers=2)
+
+    test_set = list(zip(transpose(dataset['test']['data']/255.), dataset['test']['labels']))
+    test_batches = Batches(test_set, args.batch_size_test, shuffle=False, num_workers=2)
+
+    epsilon = (args.epsilon / 255.)
+    pgd_alpha = (args.pgd_alpha / 255.)
+
+    if args.model == 'PreActResNet18':
+        model = PreActResNet18()
+    elif args.model == 'WideResNet':
+        model = WideResNet(34, 10, widen_factor=args.width_factor, dropRate=0.0)
+    else:
+        raise ValueError("Unknown model")
+
+    model = nn.DataParallel(model).cuda()
+
+    
+    params = model.parameters()
+
+    opt = torch.optim.SGD(params, lr=args.lr_max, momentum=0.9, weight_decay=5e-4)
+    criterion = nn.CrossEntropyLoss()
+    
+    epochs = args.epochs
+
+    if args.lr_schedule == 'piecewise':
+        def lr_schedule(t):
+            if t / args.epochs < 0.5:
+                return args.lr_max
+            elif t / args.epochs < 0.75:
+                return args.lr_max / 10.
+            else:
+                return args.lr_max / 100.
+
+    best_test_robust_acc = 0
+    if args.resume:
+        start_epoch = args.resume
+        model.load_state_dict(torch.load(os.path.join(args.fname, f'model_{start_epoch-1}.pth')))
+        opt.load_state_dict(torch.load(os.path.join(args.fname, f'opt_{start_epoch-1}.pth')))
+        logger.info(f'Resuming at epoch {start_epoch}')
+
+        if os.path.exists(os.path.join(args.fname, f'model_best.pth')):
+            best_test_robust_acc = torch.load(os.path.join(args.fname, f'model_best.pth'))['test_robust_acc']
+    else:
+        start_epoch = 0
+
+    if args.eval:
+        if not args.resume:
+            logger.info("No model loaded to evaluate, specify with --resume FNAME")
+            return
+        logger.info("[Evaluation mode]")
+
+    logger.info('Epoch \t Train Time \t Test Time \t LR')
+    for epoch in range(start_epoch, epochs):
+        start_time = time.time()
+        ignore_x = 0
+        ignore_x_1 = 0
+        ignore_x_2 = 0
+        ignore_x_3 = 0
+        ignore_x_4 = 0
+        ignore_x_5 = 0
+        ignore_x_6 = 0
+        ignore_x_7 = 0
+        ignore_x_8 = 0
+        ignore_x_9 = 0
+        ignore_x_10 = 0
+        ignore_x_11 = 0
+        total_x = 0
+        for i, batch in enumerate(train_batches):
+            if args.eval:
+                break
+            X, y = batch['input'], batch['target']
+            lr = lr_schedule(epoch + (i + 1) / len(train_batches))
+            opt.param_groups[0].update(lr=lr)
+
+            if args.attack == 'pgd':
+                # Random initialization
+                delta = attack_pgd(model, X, y, epsilon, pgd_alpha, args.attack_iters, args.restarts, args.norm)
+                delta = delta.detach()
+            # Standard training
+            elif args.attack == 'none':
+                delta = torch.zeros_like(X)
+            X_adv = normalize(torch.clamp(X + delta[:X.size(0)], min=lower_limit, max=upper_limit))
+
+            model.train()
+
+            robust_output = model(X_adv)
+            
+            robust_loss = nn.CrossEntropyLoss(reduce=False)(robust_output, y)
+            scale_rate = (1.5 / robust_loss).cuda().type(torch.cuda.FloatTensor)
+            Indicator_above = (scale_rate <= 1).cuda().type(torch.cuda.FloatTensor)
+            Indicator_below = (scale_rate > 1).cuda().type(torch.cuda.FloatTensor)
+            loss_scale_rate = scale_rate * Indicator_below + Indicator_above
+            robust_loss = robust_loss.mul(loss_scale_rate)
+            Indicator_ignore_1 = (robust_loss < 0.5).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_2 = ((robust_loss >= 0.5)*(robust_loss < 1.0)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_3 = ((robust_loss >= 1.0)*(robust_loss < 1.5)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_4 = ((robust_loss >= 1.5)*(robust_loss < 2.0)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_5 = ((robust_loss >= 2.0)*(robust_loss < 2.5)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_6 = ((robust_loss >= 2.5)*(robust_loss < 3.0)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_7 = ((robust_loss >= 3.0)*(robust_loss < 3.5)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_8 = ((robust_loss >= 3.5)*(robust_loss < 4.0)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_9 = ((robust_loss >= 4.0)*(robust_loss < 4.5)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_10 = ((robust_loss >= 4.5)*(robust_loss < 5.0)).cuda().type(torch.cuda.FloatTensor)
+            Indicator_ignore_11 = (robust_loss >= 5.0).cuda().type(torch.cuda.FloatTensor)
+            robust_loss = robust_loss.mean()
+
+            opt.zero_grad()
+            robust_loss.backward()
+            opt.step()
+
+            ignore_x += Indicator_below.sum().item()
+            ignore_x_1 += Indicator_ignore_1.sum().item()
+            ignore_x_2 += Indicator_ignore_2.sum().item()
+            ignore_x_3 += Indicator_ignore_3.sum().item()
+            ignore_x_4 += Indicator_ignore_4.sum().item()
+            ignore_x_5 += Indicator_ignore_5.sum().item()
+            ignore_x_6 += Indicator_ignore_6.sum().item()
+            ignore_x_7 += Indicator_ignore_7.sum().item()
+            ignore_x_8 += Indicator_ignore_8.sum().item()
+            ignore_x_9 += Indicator_ignore_9.sum().item()
+            ignore_x_10 += Indicator_ignore_10.sum().item()
+            ignore_x_11 += Indicator_ignore_11.sum().item()
+            total_x += y.size(0)
+
+        train_time = time.time()
+
+        model.eval()
+        train_loss = 0
+        train_acc = 0
+        train_robust_loss = 0
+        train_robust_acc = 0
+        train_n = 0
+        for i, batch in enumerate(train_batches):
+            X, y = batch['input'], batch['target']
+
+            # Random initialization
+            if args.attack == 'none':
+                delta = torch.zeros_like(X)
+            else:
+                delta = attack_pgd(model, X, y, epsilon, pgd_alpha, args.attack_iters, args.restarts, args.norm, early_stop=args.eval)
+            delta = delta.detach()
+
+            robust_output = model(normalize(torch.clamp(X + delta[:X.size(0)], min=lower_limit, max=upper_limit)))
+            robust_loss = criterion(robust_output, y)
+
+            output = model(normalize(X))
+            loss = criterion(output, y)
+
+            train_robust_loss += robust_loss.item() * y.size(0)
+            train_robust_acc += (robust_output.max(1)[1] == y).sum().item()
+            train_loss += loss.item() * y.size(0)
+            train_acc += (output.max(1)[1] == y).sum().item()
+            train_n += y.size(0)
+
+
+        test_loss = 0
+        test_acc = 0
+        test_robust_loss = 0
+        test_robust_acc = 0
+        test_n = 0
+        for i, batch in enumerate(test_batches):
+            X, y = batch['input'], batch['target']
+
+            # Random initialization
+            if args.attack == 'none':
+                delta = torch.zeros_like(X)
+            else:
+                delta = attack_pgd(model, X, y, epsilon, pgd_alpha, args.attack_iters_test, args.restarts, args.norm, early_stop=args.eval)
+            delta = delta.detach()
+
+            robust_output = model(normalize(torch.clamp(X + delta[:X.size(0)], min=lower_limit, max=upper_limit)))
+            robust_loss = criterion(robust_output, y)
+
+            output = model(normalize(X))
+            loss = criterion(output, y)
+
+            test_robust_loss += robust_loss.item() * y.size(0)
+            test_robust_acc += (robust_output.max(1)[1] == y).sum().item()
+            test_loss += loss.item() * y.size(0)
+            test_acc += (output.max(1)[1] == y).sum().item()
+            test_n += y.size(0)
+
+        test_time = time.time()
+
+        if not args.eval:
+            logger.info('%d \t %.1f \t \t %.1f \t \t %.4f \t %.4f \t %.4f \t %.4f \t \t %.4f \t \t %.4f \t %.4f \t %.4f \t \t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f \t\t %.4f',
+                epoch, train_time - start_time, test_time - train_time, lr,
+                train_loss/train_n, train_acc/train_n, train_robust_loss/train_n, train_robust_acc/train_n,
+                test_loss/test_n, test_acc/test_n, test_robust_loss/test_n, test_robust_acc/test_n,
+                ignore_x/total_x,
+                ignore_x_1/total_x, ignore_x_2/total_x, ignore_x_3/total_x, ignore_x_4/total_x, ignore_x_5/total_x, ignore_x_6/total_x,
+                ignore_x_7/total_x, ignore_x_8/total_x, ignore_x_9/total_x, ignore_x_10/total_x, ignore_x_11/total_x)
+
+            # save checkpoint
+            if (epoch+1) % args.chkpt_iters == 0 or epoch+1 == epochs:
+                torch.save(model.state_dict(), os.path.join(args.fname, f'model_{epoch}.pth'))
+                torch.save(opt.state_dict(), os.path.join(args.fname, f'opt_{epoch}.pth'))
+
+            # save best
+            if test_robust_acc/test_n > best_test_robust_acc:
+                torch.save({
+                        'state_dict':model.state_dict(),
+                        'test_robust_acc':test_robust_acc/test_n,
+                        'test_robust_loss':test_robust_loss/test_n,
+                        'test_loss':test_loss/test_n,
+                        'test_acc':test_acc/test_n,
+                    }, os.path.join(args.fname, f'model_best.pth'))
+                best_test_robust_acc = test_robust_acc/test_n
+        else:
+            logger.info('%d \t %.1f \t \t %.1f \t \t %.4f \t %.4f \t %.4f \t %.4f \t \t %.4f \t \t %.4f \t %.4f \t %.4f \t \t %.4f',
+                epoch, train_time - start_time, test_time - train_time, -1,
+                -1, -1, -1, -1,
+                test_loss/test_n, test_acc/test_n, test_robust_loss/test_n, test_robust_acc/test_n)
+            return
+
+
+if __name__ == "__main__":
+    main()
